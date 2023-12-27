7eedf3ee86b9a0cc430dd9812190e845eebee783
Naiem-Devs <dnsnaiem@gmail.com>
2023-12-27 11:51:57 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+
+  // page Animation
+  AOS.init({
+    mirror: true,
+    duration: 1500,
+    initClassName: 'aos-init',
+    once: true,
+  });
+
+  // data-aos="fade-up" 
+  // data-aos-delay="100" 
+
+ 
+})(jQuery);
